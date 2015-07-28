4114419f8906277653120ff84347cd3f7d4800b5
Chelsea Worrel <chelseaworrel@gmail.com>
2015-07-28 08:14:53 -0600

    relationships between merchant and items

--- config/routes.rb
+++ config/routes.rb
@@ -5,22 +5,30 @@ Rails.application.routes.draw do
       get '/customers/find_all', to: 'customers#find_all'
       get '/customers/random', to: 'customers#random'
       resources :customers, except: [:new, :edit]
+
       get '/merchants/find', to: 'merchants#find'
       get '/merchants/find_all', to: 'merchants#find_all'
       get '/merchants/random', to: 'merchants#random'
-      resources :merchants, except: [:new, :edit]
+      resources :merchants, except: [:edit, :new] do
+       get '/items',    to: "merchants/items#index"
+       get '/invoices', to: "merchants/invoices#index"
+     end
+
       get '/invoices/find', to: 'invoices#find'
       get '/invoices/find_all', to: 'invoices#find_all'
       get '/invoices/random', to: 'invoices#random'
       resources :invoices, except: [:new, :edit]
+
       get '/items/find', to: 'items#find'
       get '/items/find_all', to: 'items#find_all'
       get '/items/random', to: 'items#random'
       resources :items, except: [:new, :edit]
+
       get '/invoice_items/find', to: 'invoice_items#find'
       get '/invoice_items/find_all', to: 'invoice_items#find_all'
       get '/invoice_items/random', to: 'invoice_items#random'
       resources :invoice_items, except: [:new, :edit]
+
       resources :transactions, except: [:new, :edit]
     end
   end
