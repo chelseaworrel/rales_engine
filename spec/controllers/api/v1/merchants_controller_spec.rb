937b7362f4e0ac4b2c3286ce1e1ee4d0c8199b88
Chelsea Worrel <chelseaworrel@gmail.com>
2015-07-29 21:59:49 -0600

    heroku debugging

--- spec/controllers/api/v1/merchants_controller_spec.rb
+++ spec/controllers/api/v1/merchants_controller_spec.rb
@@ -1,1 +1,33 @@
 require 'rails_helper'
+
+describe Api::V1::MerchantsController do
+  context '#index' do
+    it 'returns all the customers' do
+      Customer.create(first_name: 'Chelsea', last_name: 'May')
+
+      get :index, format: :json
+
+      expect(response).to have_http_status(:ok)
+      customer = JSON.parse(response.body)
+
+      customer = customer.last
+
+      expect(customer['first_name']).to eq('Chelsea')
+      expect(customer['last_name']).to eq('May')
+    end
+  end
+
+  context '#show' do
+    it 'returns a customer' do
+      customer = Customer.create(first_name: 'Chelsea', last_name: 'May')
+
+      get :show, id: customer.id, format: :json
+
+      expect(response).to have_http_status(:ok)
+      customer = JSON.parse(response.body)
+
+      expect(customer['first_name']).to eq('Chelsea')
+      expect(customer['last_name']).to eq('May')
+    end
+  end
+end
