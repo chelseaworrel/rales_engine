aa57906210187766a1c8668cbfc5ff8e49c244f7
Chelsea Worrel <chelseaworrel@gmail.com>
2015-07-29 22:33:36 -0600

    model tests

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,7 +1,45 @@
 require 'rails_helper'
 
-class TransactionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Transaction, type: :model do
+  let(:customer_attributes) do
+    {first_name: "Ashley",
+     last_name: "May",
+     created_at: "21015-07-29 10:32:45",
+     updated_at: "21015-07-29 10:32:45"}
+  end
+
+  let(:merchant_attributes) do
+    {name: "Party City",
+     created_at: "21015-07-29 10:32:45",
+     updated_at: "21015-07-29 10:32:45"}
+  end
+
+  let(:invoice_attributes) do
+    {customer_id: customer.id,
+     merchant_id: merchant.id,
+     status: "shipped",
+     created_at: "21015-07-29 10:32:45",
+     updated_at: "21015-07-29 10:32:45"}
+  end
+
+  let(:transaction_attributes) do
+    {invoice_id: invoice.id,
+     credit_card_number: "4140149827486249",
+     result: "success",
+     created_at: "21015-07-29 10:32:45",
+     updated_at: "21015-07-29 10:32:45"}
+  end
+
+  let(:invoice)     { Invoice.create(invoice_attributes)        }
+  let(:customer)    { Customer.create(customer_attributes)      }
+  let(:merchant)    { Merchant.create(merchant_attributes)      }
+  let(:transaction) { Transaction.create(transaction_attributes)}
+
+  it 'responds to invoice' do
+    expect(transaction).to respond_to(:invoice)
+  end
+
+  it 'has an invoice' do
+    expect(transaction.invoice).to eq(invoice)
+  end
 end
